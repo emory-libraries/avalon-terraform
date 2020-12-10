e02d27637ee4b4cbc94ebc148dfa47fdec098ec9
Solomon Hilliard <solomon.hilliard@emory.edu>
2020-12-10 15:14:53 -0500

    Add aws variables to readme

--- variables-01-aws.tf
+++ variables-01-aws.tf
@@ -152,12 +152,12 @@ locals {
 
 variable "vpc_id" {
   type = string
-  description = "VPC that will be used by terraform, this VPC is called via data only, terraform will not attempt to manaage the existance of the VPC"
+  description = "VPC that will be used by terraform, this VPC is called via data only, terraform will not attempt to manage the existence of the VPC"
 }
 
 variable "subnet_tags" {
   type = list(string)
-  description = "List of Subnet Name tags, these subnets should exist in the provided VPC and there need to be a minimum of two in different availablity zones."
+  description = "List of Subnet Name tags, these subnets should exist in the provided VPC and there need to be a minimum of two in different availability zones."
 }
 
 variable "private_key_file" {
