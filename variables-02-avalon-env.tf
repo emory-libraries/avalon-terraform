1c3f4ef2688325ac1004a549abf498ea9f64dab1
Solomon Hilliard <solomon.hilliard@emory.edu>
2020-12-09 19:52:37 -0500

    Add more descriptions

--- variables-02-avalon-env.tf
+++ variables-02-avalon-env.tf
@@ -1,10 +1,12 @@
 variable "admin_ldap_groups" {
   type = list(string)
   default = []
+  description = "Comma seperated list of LDAP Groups, a user who is a member on any list will login as an administrator."
 }
 
 variable "assertion_cs_url" {
   default = ""
+  description = "The URL at which the SAML assertion should be received. If not provided, defaults to the OmniAuth callback URL"
 }
 
 variable "assertion_logout_url" {
@@ -24,26 +26,31 @@ variable "fedora_ssl" {
 
 variable "idp_slo_target_url" {
   default = ""
+  description = "The URL to which the single logout request and response should be sent. This would be on the identity provider. "
 }
 
 variable "idp_sso_target_url" {
   default = ""
+  description = "The URL to which the authentication request should be sent. This would be on the identity provider."
 }
 
 variable "issuer" {
   default = ""
+  description = "The name of your application. Some identity providers might need this to establish the identity of the service provider requesting the login. Also know as EntityID"
 }
 
 variable "idp_cert_file" {
-  default = ""
+  default = "Path to IDP's cert, cert should be in PEM format."
 }
 
 variable "lti_auth_key" {
   default = ""
+  description = "This LTI value is the 'username', it identifies the service to Canvas"
 }
 
 variable "lti_auth_secret" {
   default = ""
+  description = "This LTI value is the 'password'"
 }
 
 variable "secret_key_base" {
@@ -51,9 +58,11 @@ variable "secret_key_base" {
 }
 variable "sp_cert_file" {
   default = ""
+  description = "Path to the service provider's cert, in standard PEM format."
 }
 
 variable "sp_key_file" {
   default = ""
+  description = "Path to the service provider's private key, in standard PEM format."
 }
 
